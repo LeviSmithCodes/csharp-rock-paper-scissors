f27727f14eb9678dd54aba600764824de26023cc
LeviSmithCodes <eyevel@gmail.com>
2020-01-23 13:33:30 -0700

    Initial setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_rock_paper_scissors
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World! This is a modification");
+    }
+  }
+}
